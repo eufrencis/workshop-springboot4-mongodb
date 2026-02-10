a21b92efae1f5f4181de32bf96ddc3a3b6e99fe9
Francis Ferreira <francis_patricio_@hotmail.com>
2026-02-10 17:36:07 -0300

    Feat: Atualizacao de usuario com put

--- src/main/java/com/nelio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelio/workshopmongo/resources/UserResource.java
@@ -59,6 +59,16 @@ public class UserResource {
 
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity <Void> update (@RequestBody UserDTO objDto, @PathVariable String id){
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return  ResponseEntity.noContent().build();
+
+
+    }
+
 
 
 }
--- src/main/java/com/nelio/workshopmongo/service/UserService.java
+++ src/main/java/com/nelio/workshopmongo/service/UserService.java
@@ -37,9 +37,23 @@ public class UserService {
         repo.deleteById(id);
     }
 
+    public User update(User obj){
+        User entity = findById(obj.getId());
+        updateData(entity, obj);
+        return repo.save(entity);
+
+    }
+
+    public void updateData (User entity, User obj){
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+    }
+
     public User fromDTO(UserDTO objDTO) {
         return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 
 }
+
+
 
